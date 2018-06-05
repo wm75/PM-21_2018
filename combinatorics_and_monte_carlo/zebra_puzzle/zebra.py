bc7aa6f863a7e64690eeda2ce34223b024374060
Wolfgang Maier <wolfgang.maier@biologie.uni-freiburg.de>
2018-06-05 23:44:16 +0200

    Add template for solving the zebra puzzle

---
+++ combinatorics_and_monte_carlo/zebra_puzzle/zebra.py
@@ -0,0 +1,9 @@
+from itertools import permutations
+
+
+colors = ['red', 'green', 'ivory', 'yellow', 'blue']
+nationalities = ['English', 'Spanish', 'Ukrainian', 'Japanese', 'Norwegian']
+pets = ['dog', 'snails', 'fox', 'horse', 'zebra']
+drinks = ['coffee', 'milk', 'tea', 'orange juice', 'water']
+cigarettes = ['Old Gold', 'Kools', 'Chesterfields', 'Lucky Strike', 'Parliaments']
+
