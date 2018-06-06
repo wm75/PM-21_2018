45f161347db8a026c79836d673b74a71ed19e07f
Wolfgang Maier <wolfgang.maier@biologie.uni-freiburg.de>
2018-06-06 22:27:39 +0200

    Sample solution for Perseus task

--- combinatorics_and_monte_carlo/perseus_aufgabe.py
+++ combinatorics_and_monte_carlo/perseus_aufgabe.py
@@ -1,23 +1,26 @@
-# Perseus, Pegasus und Andromeda: eine Variante des Ziegenproblems
-#
-# siehe Seite 7 in:
-# www.school-scout.de/vorschau/62770/mathematische-spiele-und-strategien-22012.pdf
-
 import random
 
 # set up the three caves
 caves = [1,2,3]
-# initialize counters for the two possible outcomes
+# initialze counters for the two possible outcomes
 counter_andromeda_in_original_cave = 0
 counter_andromeda_in_third_cave = 0
 
 trials = 10000
 for trial in range(trials):
-    # simulate and count outcomes
-    # TO DO!!
-    pass
+    andromeda_in = random.choice(caves)
+    perseus_guess = random.choice(caves)
+    remaining_caves = [
+        c for c in caves if c not in (andromeda_in, perseus_guess)
+        ]
+    pegasus_hint = random.choice(remaining_caves)
+    if andromeda_in == perseus_guess:
+        counter_andromeda_in_original_cave += 1
+    elif andromeda_in == pegasus_hint:
+        raise RuntimeError("Oops, this shouldn't have happened!")
+    else:
+        counter_andromeda_in_third_cave += 1
 
-# report results of simulation
 print(
     'Reconsidering his original choice would have been good for Perseus in '
     '{0} out of {1} cases.'
